cbbf7f07788380785c81a0693e141ceab0ce0368
Ha Nguyen <31268683+trungha-ngx@users.noreply.github.com>
2022-02-07 16:55:12 +1100

    Day 8: Form Wave Animation

--- d8_form_wave_animation/script.js
+++ d8_form_wave_animation/script.js
@@ -1,1 +1,12 @@
 'use strict';
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (char, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${char}</span>`
+    )
+    .join('');
+});
