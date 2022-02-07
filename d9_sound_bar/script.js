570ab1d06d36f808266fc1ac1417c9a43fea2c0e
Ha Nguyen <31268683+trungha-ngx@users.noreply.github.com>
2022-02-08 07:55:50 +1100

    Day 9: Sound Bar

---
+++ d9_sound_bar/script.js
@@ -0,0 +1,19 @@
+'use strict';
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const stopAll = () => {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+};
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+  btn.addEventListener('click', () => {
+    stopAll();
+    document.getElementById(sound).play();
+  });
+  document.getElementById('buttons').appendChild(btn);
+});
